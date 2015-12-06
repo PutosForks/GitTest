7de192bf04491f8425bace5ab395ab5a4c25b11a
markozazi <markozazi@gmail.com>
2015-12-07 00:19:10 +0100

    collections

--- java_professional/src/main/java/collections/collectionsgupta/algorithms/array/SortArrays.java
+++ java_professional/src/main/java/collections/collectionsgupta/algorithms/array/SortArrays.java
@@ -9,14 +9,14 @@ public class SortArrays {
     public static void main(String args[]) {
         int[] intArray = {20, 14, 4, 10, 5, 3};
         for (int a : intArray) System.out.print(a + " ");
+        System.out.println();
+
         Arrays.sort(intArray);
-        System.out.println();
         for (int a : intArray) System.out.print(a + " ");
         System.out.println();
+
         intArray = new int[]{20, 14, 4, 10, 5, 3};
-        for (int a : intArray) System.out.print(a + " ");
         Arrays.sort(intArray, 1, 5);
-        System.out.println();
         for (int a : intArray) System.out.print(a + " ");   //20 4 5 10 14 3
 
     }
