eab5f0d1de693cc93d22a8b55492ef15de2edc0d
502413934 <Marko.Ziza@moneta.cz>
2018-01-19 16:52:52 +0100

    commit

---
+++ mfe/src/test/java/com/example/mfe/repository/TemplateRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.example.mfe.repository;
+
+import com.example.mfe.domain.templates.Template;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TemplateRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    TemplateRepository templateRepository;
+
+    @Test
+    public void whenFindByShortName_thenReturnEmployee() {
+
+        String shortName = "templtexxx";
+
+        Template t = new Template();
+        t.setName("templtexxx");
+        t.setShortName(shortName);
+        t.setDefaultTemplate("0");
+        t.setDescription("0");
+        t.setValid(0);
+        entityManager.persist(t);
+        entityManager.flush();
+
+        // when
+        Template found = templateRepository.findByShortName(t.getShortName());
+
+        // then
+        assertThat(found.getShortName()).isEqualTo(t.getName());
+
+
+    }
+
+}
+
+
+
---
+++ mfe/src/test/java/com/example/mfe/service/TemplateServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.mfe.service;
+
+
+import com.example.mfe.domain.templates.Template;
+import com.example.mfe.repository.TemplateRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class TemplateServiceTest {
+
+    @TestConfiguration
+    static class TemplateServiceTestContextConfiguration {
+
+        @Bean
+        public TemplateService employeeService() {
+            return new TemplateService();
+        }
+    }
+
+    @Autowired
+    private TemplateService templateService;
+
+    @MockBean
+    TemplateRepository templateRepository;
+
+
+    @Before
+    public void setUp() {
+        String shortName = "templtexxx";
+
+        Template t = new Template();
+        t.setName("templtexxx");
+        t.setShortName(shortName);
+        t.setDefaultTemplate("0");
+        t.setDescription("0");
+        t.setValid(0);
+
+        Mockito.when(templateRepository.findByShortName(t.getName()))
+                .thenReturn(t);
+    }
+
+
+    @Test
+    public void whenValidName_thenEmployeeShouldBeFound() {
+        String shortName = "templtexxx";
+        Template found = templateService.findByShortName(shortName);
+
+        assertThat(found.getShortName())
+                .isEqualTo(shortName);
+    }
+
+
+
+}
