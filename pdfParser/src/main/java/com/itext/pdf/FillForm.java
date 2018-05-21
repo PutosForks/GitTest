74a74db505fe70264689a2bbb129348c92d1bedc
marko <markozazi@gmail.com>
2018-05-21 06:55:19 +0200

    parser

--- pdfParser/src/main/java/com/itext/pdf/FillForm.java
+++ pdfParser/src/main/java/com/itext/pdf/FillForm.java
@@ -1,12 +1,14 @@
 package com.itext.pdf;
 
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfReader;
-import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.forms.PdfAcroForm;
+import com.itextpdf.forms.fields.PdfButtonFormField;
 import com.itextpdf.forms.fields.PdfFormField;
+import com.itextpdf.kernel.pdf.*;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -27,18 +29,26 @@ public class FillForm {
 
         //Initialize PDF document
         PdfDocument pdf = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
-
-
+        l
         PdfAcroForm form = PdfAcroForm.getAcroForm(pdf, true);
         Map<String, PdfFormField> fields = form.getFormFields();
-        fields.get("COMPANY_NAME").setValue("Black Barry");
+        fields.get("COMPANY_NAME").setValue("Malik Petr");
         fields.get("ICO").setValue("078978745").setJustification(PdfFormField.ALIGN_CENTER);
         fields.get("DIC").setValue("5879875");
-        fields.get("VAT_PAYER").setValue("On");
-        fields.get("EMP_CNT").setValue("50");
-        fields.get("datum33").setValue("30.11.1980");
-        fields.get("doba12").setValue("2");
-        System.out.println(fields.get("doba1").getFieldName());
+        fields.get("VAT_PAYER_1").setValue("Off");
+        fields.get("EMP_CNT").setValue("3");
+        fields.get("DN").setValue("30.11.1980");
+
+        PdfButtonFormField checkField = (PdfButtonFormField) fields.get("Radio1");
+
+        String[] values = checkField.getAppearanceStates();
+        for (String value : values) {
+            System.out.println(value);
+        }
+
+        checkField.setValue("Gamma");
+        fields.get("FF").setValue("OL");
+
         System.out.println(fields.containsKey("NE1"));
         System.out.println(fields.containsKey("NE8"));
         pdf.close();
