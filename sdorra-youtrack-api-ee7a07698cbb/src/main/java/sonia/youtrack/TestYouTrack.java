58817c5fb4ec8347e8c789160804f478639c0696
marko <markozazi@gmail.com>
2018-05-31 23:39:31 +0200

    deleted

--- sdorra-youtrack-api-ee7a07698cbb/src/main/java/sonia/youtrack/TestYouTrack.java
+++
@@ -1,9 +0,0 @@
-package sonia.youtrack;
-
-public class TestYouTrack {
-
-    public static void main(String[] args) {
-        YouTrackSession session = YouTrack.createSession("https://moneta.stringdata.cz/hub", "marko.ziza@stringdata.cz", "leonardcohen");
-    }
-
-}
